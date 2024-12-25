0d12e7471107ebdd965fc9561c93558b28f74ea6
王英杰 <gc_wangyingjie@ieggtc.com>
2024-12-25 18:28:42 +0800

    补充多开逻辑，增加机器人列表接口，增加导出聊天文件接口

--- utils/service.py
+++ utils/service.py
@@ -5,29 +5,45 @@ from utils.service_type import Response, Request
 from utils.toolkit import get_latest_wechat_version
 from libs.webot import WeBot
 
-from flask import Flask, request
+from flask import Flask, request, Response as FlaskResponse
 from requests import post as http_post, get as http_get
 
 event = Event()
 app = Flask(__name__)
 app.config['TIMEOUT'] = 300
 
-bot: WeBot = None
+bot: dict[dict[WeBot, dict]] = {}
+latest_bot: WeBot | None = None
 
 
 def on_bot_start(b: WeBot):
     global bot
-    bot = b
+    global latest_bot
+    bot.setdefault(b.remote_port, {"object": b, "info": b.info})
+    latest_bot = b
     event.set()
 
 
+def on_bot_stop(b: WeBot):
+    print('bot stop')
+
+
 def start_bot(version):
-    global bot
-    bot = WeBot(
+    _bot = WeBot(
         faked_version=version,
-        on_start=on_bot_start
+        on_start=on_bot_start,
+        on_stop=on_bot_stop
     )
-    bot.run()
+    _bot.run()
+
+
+@app.after_request
+def after_request(response):
+    global latest_bot
+    if request.endpoint == 'start':
+        latest_bot = None
+        event.clear()
+    return response
 
 
 @app.route('/')
@@ -36,28 +52,36 @@ def hello_world():
     return response.json
 
 
-@app.route('/get_latest_version')
+@app.route('/api/bot/get_latest_version')
 def get_latest_version():
     response = Response(code=200, message='success', data=None)
     response.set_data(function=get_latest_wechat_version)
     return response.json
 
 
-@app.route('/start', methods=['POST'])
+@app.route('/api/bot/start', methods=['POST'])
 def start():
+    global latest_bot
     body = request.json
     version = body.get('version')
     if not version:
         version = get_latest_wechat_version()
 
     t = Thread(target=start_bot, kwargs={"version": version})
+    t.daemon = True
     t.start()
     event.wait()
-    response = Response(code=200, message='success', data={"port": bot.remote_port})
+    response = Response(code=200, message='success', data={"port": latest_bot.remote_port})
     return response.json
 
 
-@app.route('/login_heartbeat', methods=['POST'])
+@app.route('/api/bot/list', methods=['GET'])
+def bot_list():
+    response = Response(code=200, message='success', data={port: {"info": bot.get(port).get('info')} for port in bot})
+    return response.json
+
+
+@app.route('/api/bot/login_heartbeat', methods=['POST'])
 def login_heartbeat():
     body = Request(body=request.json, body_keys=['port'])
     response = Response(code=200, message='success', data=None)
@@ -81,5 +105,47 @@ def login_heartbeat():
     return response.json
 
 
+@app.route('/api/bot/export_message_file', methods=['POST'])
+def export_message_file():
+    body = Request(body=request.json, body_keys=['port', 'wxid'])
+    response = Response(code=200, message='success', data=None)
+    if not body.check_body:
+        response.code = 400
+        response.message = '参数缺失'
+        return response.json
+
+    port = body.body.get('port')
+    wxid = body.body.get('wxid')
+
+    _bot = bot.get(int(port)).get('object')
+
+    if not _bot:
+        response.code = 400
+        response.message = '未找到对应端口的机器人'
+        return response.json
+
+    try:
+        file_path = _bot.export_message_file(
+            wxid=wxid,
+            filename=body.body.get('filename', None),
+            include_image=body.body.get('include_image', False),
+            start_time=body.body.get('start_time', None),
+            end_time=body.body.get('end_time', None),
+            export_type=body.body.get('export_type', 'json'),
+            endswith_txt=body.body.get('endswith_txt', True)
+        )
+
+        response.data = {
+            "filepath": file_path
+        }
+
+        return response.json
+
+    except Exception as e:
+        response.code = 500
+        response.message = str(e)
+        return response.json
+
+
 if __name__ == '__main__':
-    app.run(port=3569)
+    app.run(port=16001)
