a08fee2d8e74c512d68f965eac81bfc8f0d129f3
王英杰 <gc_wangyingjie@ieggtc.com>
2024-12-26 18:13:02 +0800

    增加AI对话接口

--- utils/service.py
+++ utils/service.py
@@ -1,12 +1,17 @@
-from time import sleep
+from datetime import datetime
+from json import loads
 from threading import Thread, Event
+from typing import Callable, Dict, List
 
+from libs.contact import Contact
 from utils.service_type import Response, Request
 from utils.toolkit import get_latest_wechat_version
 from libs.webot import WeBot
+from utils.chat_glm import chat_glm
+from utils.contact_captor import contact_captor
 
-from flask import Flask, request, Response as FlaskResponse
-from requests import post as http_post, get as http_get
+from flask import Flask, request
+from requests import post as http_post
 
 event = Event()
 app = Flask(__name__)
@@ -14,6 +19,27 @@ app.config['TIMEOUT'] = 300
 
 bot: dict[dict[WeBot, dict]] = {}
 latest_bot: WeBot | None = None
+
+
+def get_bot(port) -> WeBot | None:
+    return bot.get(port).get('object')
+
+
+def get_function_tools(_bot: WeBot) -> dict:
+    return {
+        "contact_captor": _bot.get_concat_from_keyword
+    }
+
+
+def chat_with_glm(ask_message, _bot: WeBot, prompt: str = None, function_tools: list = None):
+    result = chat_glm(ask_message, prompt, function_tools)
+    result = result.model_dump()
+    if result.get('tool_calls') and len(result.get('tool_calls')) > 0:
+        function_name = result.get('tool_calls')[0].get('function').get('name')
+        function_args = result.get('tool_calls')[0].get('function').get('arguments')
+        return_data = get_function_tools(_bot).get(function_name)(**loads(function_args))
+        return return_data
+    return result.get('message')
 
 
 def on_bot_start(b: WeBot):
@@ -117,7 +143,7 @@ def export_message_file():
     port = body.body.get('port')
     wxid = body.body.get('wxid')
 
-    _bot = bot.get(int(port)).get('object')
+    _bot = get_bot(int(port))
 
     if not _bot:
         response.code = 400
@@ -139,6 +165,48 @@ def export_message_file():
             "filepath": file_path
         }
 
+        return response.json
+
+    except Exception as e:
+        response.code = 500
+        response.message = str(e)
+        return response.json
+
+
+@app.route('/api/ai/chat', methods=['POST'])
+def ai_chat():
+    body = Request(body=request.json, body_keys=['port', 'messages'])
+    response = Response(code=200, message='success', data=None)
+    if not body.check_body:
+        response.code = 400
+        response.message = '参数缺失'
+        return response.json
+
+    port = body.body.get('port')
+
+    _bot = get_bot(int(port))
+
+    if not _bot:
+        response.code = 400
+        response.message = '未找到对应端口的机器人'
+        return response.json
+
+    try:
+        with open(r'../config/function_tools.json', 'r', encoding='utf-8') as f:
+            tools = loads(f.read())
+
+        result = chat_with_glm(
+            ask_message=body.body.get('messages'),
+            prompt=f"""
+        你是一个微信机器人助手，你的职责如下：
+        - 今天是{datetime.now()}。
+        - 总结用户的聊天内容，并给出回答；
+        - 分析用户的需求，按需调用tools函数；
+        """,
+            function_tools=tools,
+            _bot=_bot
+        )
+        response.data = result
         return response.json
 
     except Exception as e:
