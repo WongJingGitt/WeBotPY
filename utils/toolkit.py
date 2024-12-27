ad7774acce1e95b050fdf48a180b9278c9c8b82c
WongJingGit <WongJingGit@163.com>
2024-12-28 06:00:33 +0800

    feat(utils): 添加项目路径配置- 在 toolkit.py 中添加了项目路径配置常量 - 包括 UTILS_PATH、ROOT_PATH、DATA_PATH 和 CONFIG_PATH -这些路径配置将有助于在项目中统一管理文件路径

--- utils/toolkit.py
+++ utils/toolkit.py
@@ -1,6 +1,13 @@
+from os import path
+
 import requests
 from bs4 import BeautifulSoup
 from typing import Optional
+
+UTILS_PATH = path.dirname(path.abspath(__file__))
+ROOT_PATH = path.dirname(UTILS_PATH)
+DATA_PATH = path.join(ROOT_PATH, 'data')
+CONFIG_PATH = path.join(ROOT_PATH, 'config')
 
 
 def get_latest_wechat_version() -> Optional[str]:
