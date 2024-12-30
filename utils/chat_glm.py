5ddf4a0305306e6d81e92ee0437393dbc17d86f8
王英杰 <gc_wangyingjie@ieggtc.com>
2024-12-30 18:16:39 +0800

    feat(utils/chat_glm.py):增加对话摘要功能并优化聊天记录存储
    - 添加了将聊天记录存储到本地数据库的功能- 实现了对话摘要功能，将长对话内容进行总结
    - 优化了聊天记录的展示和存储逻辑，提高了用户体验

--- utils/chat_glm.py
+++ utils/chat_glm.py
@@ -1,9 +1,12 @@
+import json
+from datetime import datetime
 from os import environ
 from pathlib import Path
 from json import loads
 
 from libs.webot import WeBot
 from utils.toolkit import get_function_tools
+from utils.local_database import ConversationsDatabase
 
 from zhipuai import ZhipuAI
 
@@ -60,7 +63,7 @@ def chat_with_file(file_path):
     return response.choices[0].message
 
 
-def chat_with_function_tools(ask_message: list[dict], _bot: WeBot, prompt: str = None, function_tools: list = None):
+def chat_with_function_tools(ask_message: list[dict], _bot: WeBot, prompt: str = None, function_tools: list = None, conversation_id: int = None):
     """
     使用glm-4-flash模型，结合函数工具进行聊天
 
@@ -68,6 +71,7 @@ def chat_with_function_tools(ask_message: list[dict], _bot: WeBot, prompt: str =
     :param _bot: WeBot对象
     :param prompt: 对机器人的人设。
     :param function_tools: 工具函数
+    :param conversation_id:
     :return: 返回字符串。如果是工具函数返回的内容，在工具函数内需要返回一个字典，并且把需要给前端的消息字符串放在"data"字段。
     """
 
@@ -78,17 +82,21 @@ def chat_with_function_tools(ask_message: list[dict], _bot: WeBot, prompt: str =
         function_args = result.get('tool_calls')[0].get('function').get('arguments')
         return_data = get_function_tools(_bot).get(function_name)(**loads(function_args))
         if function_name == 'message_summary' and return_data.get('type') == 'prompt':
+            cdb = ConversationsDatabase()
             ask_message.append({"role": "assistant",
                                 "content": f"好的，我获取到了下面这份聊天记录:  \n  {return_data.get('data')}  \n  你需要的是这个吗？"})
             ask_message.append({"role": "user", "content": "是的，没错。"})
+
+            cdb.add_message(conversation_id, "assistant", "好的，我获取到了下面这份聊天记录:  \n  {{聊天记录}}  \n  你需要的是这个吗？", datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 0, json.dumps({"function_name": function_name, "function_args": function_args}) )
+            cdb.add_message(conversation_id, "user", "是的，没错。", datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 0)
             # TODO：
-            #  现在的问题是，加了这两行对话历史，不会返回给前端，用完就没了。后续的上下文中无法读取到这段聊天记录。
-            #  目前的计划是，完整的上下文，包括这两条存到数据库，然后加一个字段判断是否应该展示在前端。
-            #  每次前端请求对话时以完整的上下文发送请求，但是前端获取展示时过滤掉这两条。
-            #  得考虑上下文中聊天记录的存储方式，如果直接存储在上下文字段中，存在表里会不会太长了？最大可能会出现十几兆的数据？
-            #  或者可以考虑存储参数到一个新的字段，例如这条对话涉及到了什么参数，然后前端请求时再根据参数重新获取对应的聊天记录。
+            #  有bug，总结的时候会直接吧聊天记录返回来，不会总结
+
             summary_result = chat_glm(ask_message, function_tools=function_tools, prompt=prompt).model_dump().get(
                 'content')
+            print(summary_result)
             return summary_result
         return return_data.get('data')
     return result.get('content')
+
+
