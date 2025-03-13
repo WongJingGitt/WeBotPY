d936af7b84cfc17b36ec2e3a4d22483e08ac4439
王英杰 <gc_wangyingjie@ieggtc.com>
2025-03-13 19:04:26 +0800

    refactor: 移除 __init__.py 文件中的冗余代码- 删除了 bot、databases、services 和 utils 目录下的 __init__.py 文件中相同的冗余代码块 - 这些代码块试图将父目录添加到 sys.path，但在现代 Python 环境中通常是不必要的

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,3 +1,1 @@
-from sys import path as sys_path
-from os import path
-sys_path.append(path.abspath(path.dirname(path.dirname(__file__))))
+
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,2 @@
-from sys import path as sys_path
-from os import path
-sys_path.append(path.abspath(path.dirname(path.dirname(__file__))))
+
 
