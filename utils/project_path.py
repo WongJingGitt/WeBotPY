c50534fb7185f77df2ddeb6912a1c85124ab2d5d
王英杰 <gc_wangyingjie@ieggtc.com>
2025-03-13 19:06:00 +0800

    initutils: 初始化项目路径和目录结构
    - 新增代码以创建必要的目录结构
    - 在项目初始化时创建 data 和 config 目录
    - 在 data 目录下创建 databases、images 和 exports 子目录

--- utils/project_path.py
+++ utils/project_path.py
@@ -1,6 +1,15 @@
-from os import path
+from os import path, mkdir
 
 UTILS_PATH = path.dirname(path.abspath(__file__))
 ROOT_PATH = path.dirname(UTILS_PATH)
 DATA_PATH = path.join(ROOT_PATH, 'data')
 CONFIG_PATH = path.join(ROOT_PATH, 'config')
+
+if not path.exists(DATA_PATH): 
+    mkdir(DATA_PATH)
+    mkdir(path.join(DATA_PATH, 'databases'))
+    mkdir(path.join(DATA_PATH, 'images'))
+    mkdir(path.join(DATA_PATH, 'exports'))
+
+if not path.exists(CONFIG_PATH): mkdir(CONFIG_PATH)
+
