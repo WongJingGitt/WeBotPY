391911ec68b1a1c9f8d6dfee546644e8d17e3eb6
王英杰 <gc_wangyingjie@ieggtc.com>
2025-03-17 18:56:13 +0800

    build(Webot): 添加项目依赖项
    - 在 setup.py 中添加了项目所需的第三方库
    - 生成了 requires.txt 文件，列出了所有依赖项
    - 更新了 PKG-INFO 和 SOURCES.txt 文件，包含了新的依赖信息

--- setup.py
+++ setup.py
@@ -4,5 +4,18 @@ setup(
     name="Webot",
     version="0.1.0",
     packages=find_packages(),
-    include_package_data=True
+    include_package_data=True,
+    install_requires=[
+        "flask",
+        "dotenv",
+        "langchain_openai",
+        "langchain_google_genai",
+        "beautifulsoup4",
+        "lxml",
+        "wxhook",
+        "python-docx",
+        "zhipuai",
+        "langgraph",
+        "flask_cors"
+    ]
 )
