bfa56db1619d4a2acc0653e3fda52515e0b60534
王英杰 <gc_wangyingjie@ieggtc.com>
2025-03-18 17:59:57 +0800

    refactor(agent): 重构 Agent 类并移除未使用的代码
    - 删除了未使用的导入语句和类型定义
    - 移除了 agent.py 中的 main_agent、chat_agent、plan_agent 和 tool_agent 函数
    - 删除了 agent_types.py 中的 StepItem 类
    - 在 agent.py 中添加了一个新的 BaseAgent 类，但尚未实现具体功能

--- agent/agent.py
+++ agent/agent.py
@@ -1,16 +1,12 @@
-from typing import List, Dict, TypedDict
+from typing import List, Dict
 
 from langgraph.prebuilt import create_react_agent
-from langgraph.graph import StateGraph, MessagesState, START, END
-from langgraph.graph.graph import CompiledGraph
 from langgraph.checkpoint.memory import MemorySaver
-from langgraph.types import Command, Send
-from langchain_text_splitters import RecursiveJsonSplitter
+
 
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage, BaseMessage
 
 from llm.llm import LLMFactory
-from agent_types import TaskStatus, StepItem
 from tool_call.tools import ALL_TOOLS
 
 BASE_CHECKPOINTER = MemorySaver()
@@ -67,116 +63,7 @@ class WeBotAgent:
         return self.agent.stream(message, stream_mode=['updates'], config={"configurable": {"thread_id": 42}})
     
 
+class BaseAgent:
 
-if __name__ == '__main__':
-    class BaseState(TypedDict):
-        messages: MessagesState
-        model_name: str
-        llm_options: dict
-        webot_port: int = 19001
-
-
-    # TODO：
-    #   重新规划整体的Agent架构，结合StateGraph实现多Agent协同
-    #   创建聊天记录拆分Agent，传入主Prompt，根据主Prompt提取关键信息，最后创建专门用作总结的Agent，把所有分批次总结的Prompt聚合起来总结最终的报告。以规避API输入长度限制的问题。
-
-    def main_agent(state: BaseState) -> Command:
-        _prompt = """
-    你是一位专业的意图分析大师，你需要根据用户的输入分析出用户的意图，你的主要职责如下：
-    1. 分析用户的意图，并根据意图做出回复
-    2. 你的回复仅限于：chat/tool，你不负责任何工具的调用。
-    3. 当识别到用户的意图只是闲聊时，你应该回复：chat
-    4. 但识别到用户的意图中需要使用工具时，你应该回复：tool
-    """
-        _llm = LLMFactory.llm(model_name=state['model_name'], **state['llm_options'])
-        _agent = create_react_agent(
-            model=_llm,
-            checkpointer=BASE_CHECKPOINTER,
-            prompt=_prompt,
-            tools=ALL_TOOLS
-        )
-        response = _agent.invoke(state['messages'])
-        # print(response.get('messages')[-1].content)
-        if response.get('messages')[-1].content == 'chat':
-            return Send('chat_agent', state)
-        elif response.get('messages')[-1].content == 'tool':
-            return Send('tool_agent', state)
-        else:
-            return Send('chat_agent', state)
-
-
-    def chat_agent(state):
-
-        _prompt = """
-    你是一个微信机器人助手，你的职责是结合工具函数的描述解答用户任何问题，你的具体职责如下：
-    1. 你主要负责解答用户的任何问题，不论有没有涉及到工具函数的描述。
-    2. 当用户的问题涉及到工具函数时，你需要结合工具函数来解答用户的疑问。
-    3. 你仅负责回答用户的问题，不需要调用任何工具函数。
-    """
-        _llm = LLMFactory.llm(model_name=state['model_name'], **state['llm_options'])
-        _agent = create_react_agent(
-            model=_llm,
-            checkpointer=BASE_CHECKPOINTER,
-            prompt=_prompt,
-            tools=ALL_TOOLS
-        )
-        response = _agent.invoke(state['messages'])
-        state['messages']['messages'].append(response)
-        # print(response)
-        return Command(goto=END, update=state)
-
-
-    def plan_agent(state):
-        _prompt = """
-    你是一位专业的任务规划大师，你的主要职责是：
-    """
-        _llm = LLMFactory.llm(model_name=state['model_name'], **state['llm_options'])
-        _agent = create_react_agent(
-            model=_llm,
-            checkpointer=BASE_CHECKPOINTER,
-            prompt=_prompt,
-            tools=ALL_TOOLS
-        )
-        response = _agent.invoke(state['messages']['messages'])
-
-
-    def tool_agent(state):
-        _prompt = """
-    """
-        _llm = LLMFactory.llm(model_name=state['model_name'], **state['llm_options'])
-        _agent = create_react_agent(
-            model=_llm,
-            checkpointer=BASE_CHECKPOINTER,
-            prompt=_prompt,
-            tools=ALL_TOOLS
-        )
-        response = _agent.invoke(state['messages'])
-        state['messages']['messages'].append(response)
-        return Command(goto=END, update=state)
-
-
-    graph = StateGraph(
-        BaseState
-    )
-
-    graph.add_node(main_agent)
-    graph.add_node(chat_agent)
-    graph.add_node(tool_agent)
-
-    graph.add_conditional_edges(START, main_agent, ['chat_agent', 'tool_agent'])
-    # graph.add_edge
-
-    agent = graph.compile()
-
-    result = agent.invoke(
-        {
-            "messages": MessagesState(messages=[HumanMessage(content="你好, 请帮我查询我现在的微信名字是什么？")]),
-            "model_name": "gemini-2.0-flash-exp",
-            "llm_options": {
-                "apikey": "",
-                "base_url": ''
-            }
-        }
-    )
-
-    print(result)
+    def __init__(self, model_name: str = "glm-4-flash", llm_options: dict = {}, webot_port: int = 19001):
+        pass
--- agent/agent_types.py
+++ agent/agent_types.py
@@ -3,20 +3,6 @@ from typing import Literal, Dict, Any, List, TypedDict
 from langgraph.graph import MessagesState
 
 
-class StepItem(BaseModel):
-    step_id: int = Field(..., description='步骤的唯一标识符，用于标识当前步骤在任务中的顺序和引用。')
-    description: str = Field(..., description='对当前步骤的简要描述，显示给用户，帮助理解该步骤的目的和任务。')
-    tool: str = Field(..., description='执行该步骤所调用的工具函数名称。')
-    input: Dict[str, Any] = Field(default={}, description='调用工具函数时所需要的输入参数，格式为字典。')
-    depends_on: List[int] = Field(default=[], description='该步骤依赖的其他步骤的ID列表，用于定义执行顺序和依赖关系。')
-    clarification: str = Field(default='', description='当该步骤可能需要用户澄清或确认时，提供相应提示信息。')
-    decision_required: str = Field(default='',
-                                   description='描述该步骤中可能需要模型推理或任务执行专家进行自主决策的内容。')
-
-    model_config = {"json_schema_extra": {"type": "object"}}
-
-
-
 class TaskStatus(TypedDict):
     status: Literal['IN_PROGRESS', 'COMPLETED', 'NEED_CLARIFICATION']
 
